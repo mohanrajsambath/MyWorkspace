4626f2abe19c639a073cf600e99d254305f887ef
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2018-08-22 16:51:51 +0530

    Modules Created: 1.RecyclerSwipe Delete with Drag and Drop Implemented.

---
+++ recyclerviewswipereveal/src/main/java/mohan/com/recyclerviewswipereveal/MainActivity.java
@@ -0,0 +1,71 @@
+package mohan.com.recyclerviewswipereveal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mohan.com.utils.OnMenuListChangedListener;
+import mohan.com.utils.OnStartDragListener;
+import mohan.com.utils.SimpleItemTouchHelperCallback;
+
+public class MainActivity extends AppCompatActivity  implements OnMenuListChangedListener,OnStartDragListener {
+    private ItemTouchHelper mItemTouchHelper;
+    private OnStartDragListener mDragStartListener;
+    private OnMenuListChangedListener mListChangedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        MainListAdapter mainListAdapter = new MainListAdapter(getMealList(),this,this,this);
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mainListAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+        /*mRecyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(this)
+                .colorResId(R.color.colorPrimaryDark)
+                .size(2)
+                .build());*/
+        recyclerView.setAdapter(mainListAdapter);
+    }
+
+    public List<String> getMealList() {
+        List<String> mealList = new ArrayList<>();
+        mealList.add("Green Thai Curry");
+        mealList.add("Granola");
+        mealList.add("Poached Eggs");
+        mealList.add("Spaghetti");
+        mealList.add("Apple Pie");
+        mealList.add("Grilled Cheese Sandwich");
+        mealList.add("Vegetable Soup");
+        mealList.add("Chicken Noodles");
+        mealList.add("Fajitas");
+        mealList.add("Chicken Pot Pie");
+        mealList.add("Pasta and cauliflower casserole with chicken");
+        mealList.add("Vegetable stir-fry");
+        mealList.add("Sweet potato and orange soup");
+        mealList.add("Vegetable Broth");
+        return mealList;
+    }
+
+    @Override
+    public void onNoteListChanged(List<String> foodMenus) {
+       /* List<String> resultFoodMenu=new ArrayList<String>();
+        resultFoodMenu=foodMenus;
+        for(int i=0;resultFoodMenu.size()>0;i++){
+            System.out.println("Position"+resultFoodMenu.get(i));
+        }*/
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+}
---
+++ recyclerviewswipereveal/src/main/java/mohan/com/utils/SimpleItemTouchHelperCallback.java
@@ -0,0 +1,76 @@
+package mohan.com.utils;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/*
+ * Copyright (c) 2018. Created by Mohanraj.S,Innobot Systems on 22/8/18 for MyWorkspace
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback{
+
+    private final ItemTouchHelperAdapter mAdapter;
+
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
+        mAdapter = adapter;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return false;
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        final int swipeFlags = ItemTouchHelper.END;
+        //final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder sourceViewHolder, @NonNull RecyclerView.ViewHolder targetViewHolder) {
+        mAdapter.onItemMove(sourceViewHolder.getAdapterPosition(), targetViewHolder.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+       mAdapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+            itemViewHolder.onItemSelected();
+        }
+
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+        ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+        itemViewHolder.onItemClear();
+    }
+
+
+}
