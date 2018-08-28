279f07763f5c49a8bc6ff91c9f0afb532e8ca0b9
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2018-08-28 15:39:48 +0530

    DateTimePicker New Module Incorporated.

---
+++ datetimepicker/src/main/java/mohan/com/datetimepicker/activity/MainActivity.java
@@ -0,0 +1,67 @@
+package mohan.com.datetimepicker.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Date;
+
+import mohan.com.datetimepicker.R;
+import mohan.com.datetimepicker.utils.SingleDateAndTimePicker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private TextView txtVw_date;
+    private ImageView imgVw_close_datePicker;
+    private FrameLayout fL_date_bottomSheet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtVw_date = (TextView)findViewById(R.id.txtVw_date);
+        imgVw_close_datePicker = (ImageView)findViewById( R.id.imgVw_close_datePicker);
+        fL_date_bottomSheet =(FrameLayout)findViewById(R.id.fL_date_bottomSheet);
+        txtVw_date.setOnClickListener(this);
+        imgVw_close_datePicker.setOnClickListener(this);
+
+        setDateTimePicker();
+
+
+
+    }
+
+    private void setDateTimePicker() {
+        final SingleDateAndTimePicker singleDateAndTimePicker = (SingleDateAndTimePicker) findViewById(R.id.single_day_picker);
+        singleDateAndTimePicker.addOnDateChangedListener(new SingleDateAndTimePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(String displayed, Date date) {
+                display(displayed);
+            }
+        });
+    }
+
+    private void display(String toDisplay) {
+        //Toast.makeText(this, toDisplay, Toast.LENGTH_SHORT).show();
+        txtVw_date.setText(toDisplay);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.txtVw_date:
+                fL_date_bottomSheet.setVisibility(View.VISIBLE);
+                break;
+            case R.id.imgVw_close_datePicker:
+                fL_date_bottomSheet.setVisibility(View.GONE);
+                break;
+                default:
+                    break;
+
+        }
+    }
+}
---
+++ datetimepicker/src/main/java/mohan/com/datetimepicker/widget/WheelMonthPicker.java
@@ -0,0 +1,95 @@
+package mohan.com.datetimepicker.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import static mohan.com.datetimepicker.utils.DateHelper.getMonth;
+import static mohan.com.datetimepicker.utils.DateHelper.today;
+
+public class WheelMonthPicker extends WheelPicker<String> {
+
+    private int lastScrollPosition;
+
+    private MonthSelectedListener listener;
+
+    private boolean displayMonthNumbers = false;
+
+    public WheelMonthPicker(Context context) {
+        this(context, null);
+    }
+
+    public WheelMonthPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected List<String> generateAdapterValues() {
+        final List<String> monthList = new ArrayList<>();
+
+        final SimpleDateFormat month_date = new SimpleDateFormat("MMMM", Locale.getDefault());
+        final Calendar cal = Calendar.getInstance(Locale.getDefault());
+
+        for (int i = 0; i < 12; i++) {
+            cal.set(Calendar.MONTH, i);
+            if (displayMonthNumbers) {
+                monthList.add(String.format("%02d", i + 1));
+            } else {
+                monthList.add(month_date.format(cal.getTime()));
+            }
+        }
+
+        return monthList;
+    }
+
+
+    @Override
+    protected String initDefault() {
+        return String.valueOf(getMonth(today()));
+    }
+
+    public void setOnMonthSelectedListener(MonthSelectedListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onItemSelected(int position, String item) {
+        if (listener != null) {
+            listener.onMonthSelected(this, position, item);
+        }
+    }
+
+    @Override
+    protected void onItemCurrentScroll(int position, String item) {
+        if (lastScrollPosition != position) {
+            onItemSelected(position, item);
+            lastScrollPosition = position;
+        }
+    }
+
+    public boolean displayMonthNumbers() {
+        return displayMonthNumbers;
+    }
+
+    public void setDisplayMonthNumbers(boolean displayMonthNumbers) {
+        this.displayMonthNumbers = displayMonthNumbers;
+    }
+
+    public int getCurrentMonth() {
+        return getCurrentItemPosition();
+    }
+
+    public interface MonthSelectedListener {
+        void onMonthSelected(WheelMonthPicker picker, int monthIndex, String monthName);
+    }
+}
